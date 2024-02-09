60d44241c74ef96c889b9e7f1d7b776739517942
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-02-09 20:51:31 +0100

    Extend example to work on arbitrary files
    This is useful to check that the crate still works on modern files.

--- examples/read_mtree.rs
+++ examples/read_mtree.rs
@@ -4,7 +4,11 @@ use std::error::Error;
 use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let path = env::current_dir()?.join("examples/gedit.mtree");
+    let args: Vec<_> = env::args().collect();
+    let path = match args.get(1) {
+        Some(p) => p.into(),
+        None => env::current_dir()?.join("examples/gedit.mtree"),
+    };
     let mtree = MTree::from_reader(File::open(path)?);
     for entry in mtree {
         println!("{}", entry?);
